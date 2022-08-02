6b3d3282c3345cfedb2a0ceafbf5d8e6e8eed2b9
HeSer <B@nd@r1359>
2022-08-02 10:43:14 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,83 @@
+
+# print("Hello World, Why my system doesn't work?")
+# it is good to work wiht issues that really helps.
+
+# print("Mosa Rahimi")
+# print("o____")
+# print(" ||||")
+# print("*" * 10)
+
+price = 10
+price = 20 # Integer number
+rating = 4.1 # floats
+name = "Mosa" # string
+is_published = True
+
+# print(price)
+
+# full_name = "John Smith"
+# age = 20
+# is_new_patient= True
+
+# name = input("What is your name? ")
+# print("Hi, " + name)
+
+
+# name = input("What is your name? ")
+# fav_color = input("What is your favorite Color? ")
+# print(name + " likes " + fav_color)
+
+# birth_year = input("birht year ")
+# print(type(birth_year))
+# age = 2022 - int(birth_year)
+# print(age)
+# print(type(age))
+
+# full_name = input("What is your name? ")
+# weight = input("how many pounds do you weight? " + full_name + " ")
+# kilo_gram = int(weight)/ 2.2
+# print("Ok " + full_name + "Your weight in Killogram is " + str(kilo_gram))
+
+
+# course = "Python's Course for beginners"
+# print(course)
+# print(course[2:-2])
+#
+# multi_line = """Hello John.
+# This is an example of our work.
+# I would be very happy if you let me know how to work with it."""
+# # print(multi_line)
+# print(multi_line[0:5])
+# print(multi_line[5:15])
+
+#
+# first_n = "John"
+# last_n = "Smith"
+# message = first_n + " [" + last_n + "] is a python pgoramer."
+# print(message)
+
+# first = "Mosa"
+# last = "Rahimi"
+# # formatted string,
+# msg = f'{first} [{last}] is python coder.'
+# print(msg)
+
+course = "This Course is Python for Bigginers"
+# print(len(course))
+# course.upper()
+# print(course.upper())
+# print(course.lower())
+# print(course.find("o"))
+# print(course.replace("Bigginers", "Absolute Bigginners"))
+# print(course.replace("bigginers", "Absolute Bigginners")) # Because the biggners term is not exactly with the variable
+# print("Python" in course)
+# print("python" in course)
+# print(course.title())
+
+# working with numbers and operatiors
+print(10*2)
+print(10+2)
+print(10/3)
+print(11//3)
+print(10**3)
+print(11%3)
